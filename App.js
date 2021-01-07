dc4591f8969cd5131d3d8ae85175d11f81f78f16
mgrybel <monika.grybel@hotmail.com>
2021-01-07 04:01:28 +0000

    Updated the app

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  Button, 
+  FlatList 
+} from 'react-native';
 
 export default function App() {
+  const [enteredItem, setEnteredItem] = useState('');
+  const [toDoItems, setToDoItems] = useState([]);
+
+  const itemInputHandler = (enteredText) => {
+    setEnteredItem(enteredText);
+  };
+
+  const addItemHandler = () => {
+    setToDoItems(currentItems => [
+      ...currentItems, 
+      {key: Math.random().toString(), value: enteredItem}
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="TODO Item" 
+        style={styles.input} 
+        onChangeText={itemInputHandler}
+        value={enteredItem} />
+        <Button title="ADD" onPress={addItemHandler} />
+      </View>
+      <FlatList 
+        data={toDoItems} 
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )} 
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 40
   },
+  inputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
